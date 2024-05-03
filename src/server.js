595ddcf393decafa27d3020633586771ca211748
root <suk.joanne@gmail.com>
2024-05-03 23:40:54 +0000

    halfway through site updates

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
 let express = require('express'); // express is middleware that handles connection for you
 let app = express();
 let http = require("http");
-let PORT = 80;
+let PORT = 3000;
 
-app.use(express.static('./public'));
+app.use(express.static('/root/static-site/public'));
+
+/*
 app.get('/', function (req, res) {
-
+res.send('backup site is running');
 });
+*/
 
 const httpSever = http.createServer(app);
-httpSever.listen(PORT, () => {
+httpSever.listen(PORT, '::', () => {
   console.log(`server has started on port ${PORT}`);
 });
